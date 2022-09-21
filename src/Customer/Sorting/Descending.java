b94a47cc88ccb111841baa7bc8b5d307abb9b108
s3927186 <95325780+s3927186@users.noreply.github.com>
2022-09-21 18:03:31 +0700

    Price sorting descending order(FINISH)

---
+++ src/Customer/Sorting/Descending.java
@@ -0,0 +1,35 @@
+package Customer.Sorting;
+
+//Sorting all products by price in Ascending order
+public class Descending implements Comparable<Descending>{
+    private final String itemsID;
+    private final String titles;
+    private final int intPrice;
+    private final String Category;
+
+
+    public Descending(String itemsID, String titles, String price, String Category) {
+        this.itemsID = itemsID;
+        this.titles = titles;
+        intPrice = Integer.parseInt(price);
+        this.Category = Category;
+    }
+    public String toString(){
+        return itemsID + "," + titles + "," + intPrice + "," + Category;
+    }
+
+    @Override
+    public int compareTo(Descending otherPriceSort) {
+        if (otherPriceSort == null){
+            throw new NullPointerException("Try to compare" + this + "to null");
+        }else if(!this.getClass().equals(otherPriceSort.getClass())){
+            throw new ClassCastException("maybe Classloader Issue. Failed to compare" + this + "to"+otherPriceSort);
+        }else if (this.intPrice < otherPriceSort.intPrice) {
+            return 1;
+        }else if (this.intPrice > otherPriceSort.intPrice)
+            return -1;
+
+        return 0;
+    }
+}
+
